b060a4ae45877b40b43253c8c0597ed66b755439
sanctumr <sanctumr@outlook.com>
2025-01-20 20:15:00 -0300

    solved some challenges

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def count_characters(file_contents):
 
     for char in file_contents:
         char = char.lower()
-
-        characters[char] = characters.get(char, 0) + 1
+        if char.isalpha():
+            characters[char] = characters.get(char, 0) + 1
 
     return characters
 
@@ -12,13 +12,36 @@ def count_words(file_contents):
     list_of_words = file_contents.split()
     return len(list_of_words)
 
+def get_num(dict):
+    return dict["num"]
+
 def __main__():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         # print(file_contents)
 
-    # print(count_words(file_contents))
+    word_count = count_words(file_contents)
+    characters = count_characters(file_contents)
 
-    print(count_characters(file_contents))
+    # Convert dictionary to list of dictionaries
+
+    char_list = []
+    for char,count in characters.items():
+        char_list.append({"char": char, "num": count})
+
+    # Sort the list by count (num) in descending order
+    char_list.sort(reverse=True, key=get_num)
+    
+    # Let's print to see what we have
+    # print(char_list)
+
+    # Print the report
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
+
+    for char_dict in char_list:
+        print(f"The '{char_dict['char']}' character was found {char_dict['num']} times")
+
+    print("--- End report ---")
 
 __main__()
